c09f38abf0b9a5052591ed41b73484fdda02a37f
Steve Akinfenwa <sakinfens@gmail.com>
2018-05-24 20:35:31 -0400

    lower case t in test

--- Test/server-test.js
+++ Test/server-test.js
@@ -1,8 +1,11 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+
 chai.use(chaiHttp);
 const {app, runServer, closeServer} = require('../server.js');
+
 const expect = chai.expect;
+
 describe('HomeRout', function(){
   it('should return HTML and 200 status code', function(){
     return chai.request('http://localhost:8080')
