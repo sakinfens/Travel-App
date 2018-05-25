471387a707d08547260efccee2cd8e2789fc0f65
Steve Akinfenwa <sakinfens@gmail.com>
2018-05-24 21:05:56 -0400

    trying test folder again

---
+++ test 9.01.03 PM/server-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const {app, runServer, closeServer} = require('../server.js');
+
+const expect = chai.expect;
+
+describe('HomeRout', function(){
+  it('should return HTML and 200 status code', function(){
+    return chai.request('http://localhost:8080')
+    .get('/')
+    .then(function(res){
+      expect(res).to.have.status(200);
+      expect(res).to.be.html;
+      })
+  })
+})
