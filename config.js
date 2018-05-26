ff3bf78f438ef3f27174c33539c958751f26d110
Steve Akinfenwa <sakinfens@gmail.com>
2018-05-26 13:25:28 -0400

    adding config file

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+      API_BASE_URL: 'https://stark-peak-34761.herokuapp.com/'
+}
--- test/server-test.js
+++ test/server-test.js
@@ -6,7 +6,15 @@ const {app, runServer, closeServer} = require('../server.js');
 
 const expect = chai.expect;
 
+const API_BASE_URL = require('../config.js');
 describe('HomeRout', function(){
+	//before(function() {
+    //return runServer();
+  //});
+
+  //after(function() {
+    //return closeServer();
+  //});
   it('should return HTML and 200 status code', function(){
     return chai.request('http://localhost:8080')
     .get('/')
